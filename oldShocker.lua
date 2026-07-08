64173d41a6434e91c2a64d9c00524159b125a1d6
Francisco1692qzd <franciscomine16432@gmail.com>
2026-07-08 20:49:59 -0300

    Refactor achievement unlocking logic in oldShocker.lua

--- oldShocker.lua
+++ oldShocker.lua
@@ -160,14 +160,14 @@ local function SpawnShocker()
                     -- Entity ignores player and disappears
                     mainPart.Anchored = false
                     mainPart.CanCollide = false
-                    local AchievementModule = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.AchievementUnlock
+                    --[[local AchievementModule = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.AchievementUnlock
 	                if AchievementModule == nil then return end
 	                if workspace:FindFirstChild("ShockerAchievement") then return end
 	                if not game.ReplicatedStorage:FindFirstChild("ModulesShared") then return end
 	                local dataModule = require(game:GetService("ReplicatedStorage"):WaitForChild("ModulesShared"):WaitForChild("Achievements"))
-	                local unlockFunc = require(AchievementModule)
+	                local unlockFunc = require(AchievementModule)--]]
 	                if not workspace:FindFirstChild("ShockerAchievement") then
-		                unlockFunc(nil, "Shocker") 
+		                GiveAchievement("Shocker")
 	                end
 	                local ObtainedBadge = Instance.new("BoolValue")
 	                ObtainedBadge.Name = "ShockerAchievement"
